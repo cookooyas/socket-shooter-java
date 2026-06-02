2f91d54fe7ce8927035a33efcfa2f51b48834da9
cookooyas <cookooyas@gmail.com>
2026-06-02 10:26:44 +0900

    initial commit

---
+++ src/main/java/com/game/socketshooterjava/SocketShooterJavaApplication.java
@@ -0,0 +1,13 @@
+package com.game.socketshooterjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketShooterJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketShooterJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/game/socketshooterjava/SocketShooterJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.socketshooterjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketShooterJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
